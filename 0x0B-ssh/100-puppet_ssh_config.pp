a6c7f92f7998b719e6e13a177533b389dcd6c106
eya-98 <eya.nani@sesame.com.tn>
2021-04-10 15:09:39 +0000

    ssh config

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -2,9 +2,11 @@
 file {'refuse to authenticate using a password' :
     path => '/etc/ssh/ssh_config',
     line => 'PasswordAuthentication no'
+    replace => true
 }
 
 file { 'use the private key':
     path => '/etc/ssh/ssh_config',
     line => 'IdentityFile ~/.ssh/holberton'
+    replace => true
 }
