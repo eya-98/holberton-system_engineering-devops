7141495a92fc859f66b8fee016316ed8beb2d000
eya-98 <eya.nani@sesame.com.tn>
2021-04-10 15:15:37 +0000

    ssh config

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,13 +1,12 @@
 #using Puppet to make changes to our configuration file
-file {'refuse to authenticate using a password' :
-    path => '/etc/ssh/ssh_config',
-    line => 'PasswordAuthentication no',
-    replace => true
+line { 'Turn off password':
+  path    => '/etc/ssh/ssh_config',
+  line    => 'PasswordAuthentication no',
+  replace => true
 }
 
-file { 'use the private key':
-    path => '/etc/ssh/ssh_config',
-    line => 'IdentityFile ~/.ssh/holberton',
-    replace => true
+line { 'Declare identity':
+  path    => '/etc/ssh/ssh_config',
+  line    => 'IdentityFile ~/.ssh/holberton',
+  replace => true
 }
-
