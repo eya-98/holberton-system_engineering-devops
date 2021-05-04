1d0c60b84c37963adc02ee63450ccbc42752a99c
eya-98 <eya.nani@sesame.com.tn>
2021-05-04 02:58:17 +0000

    export data in the JSON format

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""export data in the JSON format."""
+import json
+import requests
+
+if __name__ == "__main__":
+    with open('todo_all_employees.json', mode='w') as employement:
+        employees = requests.get('https://jsonplaceholder.typicode.com/users/').json()
+        details = requests.get('https://jsonplaceholder.typicode.com/users/todos').json()
+        for employee in employees:
+            ids = employee.get('id')
+            todos = requests.get('https://jsonplaceholder.typicode.com/users/{}/todos'
+                .format(ids)).json()
+            tasks = []
+            for task in todos:
+                tasks.append({"task": task.get("title"),
+                          "completed": task.get("completed"),
+                          "username": employee.get("username")})
+            json_file = {ids: tasks}
+            json.dump(json_file, employement)
+
