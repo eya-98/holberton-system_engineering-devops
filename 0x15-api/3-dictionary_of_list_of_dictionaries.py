4b72bbf702cd43e269fd57bbcb85d2f03c575301
eya-98 <eya.nani@sesame.com.tn>
2021-05-05 21:43:17 +0000

    dictionnary of list of dictionaries

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -6,16 +6,14 @@ import requests
 if __name__ == "__main__":
     with open('todo_all_employees.json', mode='w') as employement:
         employees = requests.get('https://jsonplaceholder.typicode.com/users/').json()
-        details = requests.get('https://jsonplaceholder.typicode.com/users/todos').json()
         for employee in employees:
             ids = employee.get('id')
             todos = requests.get('https://jsonplaceholder.typicode.com/users/{}/todos'
                 .format(ids)).json()
             tasks = []
             for task in todos:
-                tasks.append({"username": employee.get("username"),
-                              "task": task.get("title"),
+                tasks.append({"task": task.get("title"),
+                              "username": employee.get("username"),
                               "completed": task.get("completed")})
             json_file = {ids: tasks}
-            json.dump(json_file, employement, sort_keys=True)
-
+        json.dump(json_file, employement)
