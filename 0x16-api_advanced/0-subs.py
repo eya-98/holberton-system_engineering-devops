8ad221389f78fce06aff1d063464323416d8951b
eya-98 <eya.nani@sesame.com.tn>
2021-05-18 12:02:19 +0000

    subscribers

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,7 +6,7 @@ import requests
 def number_of_subscribers(subreddit):
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     reddit_api = requests.get(url, headers={"User-agent": "Custom"},
-                     allow_redirects=False).json()
+                              allow_redirects=False).json()
     try:
         return reddit_api["data"]["subscribers"]
     except Exception:
